295f49bc53df501cab3b305f1493d1e41a5433f3
LuuscoTheLover <127765876+LuuscoTheLover@users.noreply.github.com>
2024-03-07 15:19:26 -0300

    GoBackToProject Dumbass

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -1,47 +1,124 @@
 using Godot;
 using System;
-using Luusco;
 
-
-public partial class PlayerMovement : RigidBody3D
+public partial class PlayerMovement : Node
 {
-	enum GrabbingState {
-		Idle,
-		Grabbing,
-		Releasing
+	[ExportGroup("Atatchables")]
+	public Camera3D Head;
+	public Player Player;
+
+	[ExportGroup("Player Stats")]
+	[Export] public float WalkSpeed = 5.0f;
+	[Export] public float RunSpeed = 8.0f;
+	[Export] public float LowGravityJump = 1.0f;
+	[Export] public float NormalJump = 4.5f;
+	public float JumpVelocity = 4.5f;
+	public float AirMovementMultiplier = 0.3f;
+	private float jump;
+	private float speed;
+
+	public float gravity = 9.8f;
+	private CustomSignals _customSignal;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		Player = GetParent<Player>();
+		Head = Player.GetNode<Camera3D>("Neck/Head");
+		speed = WalkSpeed;
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+		_customSignal = GetNode<CustomSignals>("/root/CustomSignals");
+		_customSignal.NewGravity += UpdateGravity;
 	}
-	GrabbingState Grabbing;
-	public Generic6DofJoint3D joint;
-	RayCast3D ray;
-	
-    public override void _Ready()
-    {
-        base._Ready();
-		joint = GetNode<Generic6DofJoint3D>("Joint");
-		ray = GetNode<RayCast3D>("Neck/Head/RayCast3D");
-		ray.AddException(this);
 
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        base._PhysicsProcess(delta);
-		Godot.GodotObject collider = ray.GetCollider();
-		if (Input.IsActionJustPressed("click")) {
-			Grabbing = GrabbingState.Grabbing;
-		} else if (Input.IsActionJustReleased("click")) {
-			Grabbing = GrabbingState.Releasing;
-		} else {
-			Grabbing = GrabbingState.Idle;
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		if (Input.IsActionJustPressed("esc") && Input.MouseMode == Input.MouseModeEnum.Captured)
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
 		}
-
-		switch (Grabbing){
-			case GrabbingState.Grabbing:
-			break;
-			case GrabbingState.Releasing:
-			break;
-			case GrabbingState.Idle:
-			break;
+		else if (Input.IsActionJustPressed("click") && Input.MouseMode == Input.MouseModeEnum.Visible)
+		{
+			Input.MouseMode = Input.MouseModeEnum.Captured;
 		}
-    }
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Player.Velocity = MoveAndJump((float)delta);
+		SpeedJumpHandler();
+		Player.MoveAndSlide();
+	}
+
+	//Player movement, jump, and gravity	
+	public Vector3 MoveAndJump(float delta)
+	{
+		Vector3 movement = Player.Velocity;
+		movement.Y -= gravity * delta;
+
+		if (Input.IsActionJustPressed("jump") && Player.IsOnFloor())
+		{
+			movement.Y = JumpVelocity;
+		}
+		Vector2 inputDir = Input.GetVector("left", "right", "foward", "backward");
+		Vector3 direction = (Head.GlobalTransform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (Player.IsOnFloor())
+		{
+			if (direction != Vector3.Zero)
+			{
+				movement.X = direction.X * speed;
+				movement.Z = direction.Z * speed;
+			}
+			else
+			{
+				movement.X = 0f;
+				movement.Z = 0f;
+			}
+		}
+		else
+		{
+			if (direction != Vector3.Zero)
+			{
+				movement.X = direction.X * speed * AirMovementMultiplier;
+				movement.Z = direction.Z * speed * AirMovementMultiplier;
+			}
+			else if (gravity > 5.0f)
+			{
+				movement.X = Mathf.Lerp(movement.X, direction.X * speed, delta * 2.0f);
+				movement.Z = Mathf.Lerp(movement.Z, direction.Z * speed, delta * 2.0f);
+			}
+		}
+		return movement;
+	}
+
+	public void SpeedJumpHandler()
+	{
+		float newSpeed;
+		float multiplier;
+		if (gravity > 5.0f)
+		{
+			multiplier = 1f;
+			JumpVelocity = NormalJump;
+		}
+		else
+		{
+			multiplier = 0.2f;
+			JumpVelocity = LowGravityJump;
+		}
+		if (Input.IsActionPressed("run"))
+		{
+			newSpeed = RunSpeed * multiplier;
+		}
+		else
+		{
+			newSpeed = WalkSpeed * multiplier;
+		}
+		speed = newSpeed;
+	}
+
+	public void UpdateGravity(float newGrav)
+	{
+		gravity = newGrav;
+	}
 }
